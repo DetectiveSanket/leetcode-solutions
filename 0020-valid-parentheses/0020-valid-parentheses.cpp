0984ff10ac1ca6a8a627a85d16ed67401fd4d9da
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-08-02 23:08:01 +0530

    Time: 3 ms (9.08%), Space: 8.8 MB (89.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+  // Approach :-
+  // Time :- O(N);
+  // Space :- O(1);    
+        stack<char> st;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s[i];
+
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+
+            else{
+
+                if(st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if((top == '(' && ch != ')')||
+                  (top == '[' && ch != ']')||
+                  (top == '{' && ch != '}')){
+                    return false;
+                  }
+            }
+
+        }
+
+        return st.empty();
+
+    }
+};
