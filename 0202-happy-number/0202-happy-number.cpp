986170af4b61100fb63de5c8e6f8ad81ac025bf7
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-08-07 20:41:46 +0530

    Time: 1 ms (27.85%), Space: 8.3 MB (39.32%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,30 @@
+#include <unordered_set>
+
+// Approach : Hash Set
+// Time Complexity: O(log n)
+// Space Complexity: O(log n)
+
+class Solution {
+private:
+    int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+public:
+    bool isHappy(int n) {
+        std::unordered_set<int> seen;
+
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+
+        return n == 1;
+    }
+};
