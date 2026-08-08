a6ac47fe6004194b50f752054f98ee97fc24ce72
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-08-08 20:07:31 +0530

    Time: 0 ms (100%), Space: 15 MB (17.29%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+
+  // Approach :- hashing
+  // Time :- O(N);
+  // Space :- O(N);
+
+        unordered_map<int , int> mp;
+        for(auto i : nums1) {
+            mp[i]++;
+        }
+        vector<int> ans;
+
+        for(auto i : nums2) {
+            if(mp[i] > 0 ) {
+                ans.push_back(i);
+                mp[i]--;
+            }
+        }
+
+        return ans;
+
+    }
+};
