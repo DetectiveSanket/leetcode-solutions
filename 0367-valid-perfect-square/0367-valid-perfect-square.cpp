09ead1b059486843a459a0d788fcce41d30092f4
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-08-11 19:01:43 +0530

    Time: 0 ms (100%), Space: 7.7 MB (54.33%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -12,13 +12,15 @@ public:
         while(left <= right) {
 
             int mid = left + (right - left) / 2;
-            long long int sqr = (long long)mid * mid;
+            // long long int sqr = (long long)mid * mid;
 
-            if(num == sqr) {
-                return true;
+            if(mid == num / mid) { // num == sqr
+                if(num % mid == 0 ) return true;
+
+                left = mid + 1;
             }
 
-            else if (num < sqr) {
+            else if (mid > num / mid) { // num < sqr
                 right = mid - 1;
             }
 
@@ -30,3 +32,21 @@ public:
         return false;
     }
 };
+
+
+// int left = 1;
+// int right = num;
+// while (left <= right) {
+//     int mid = left + (right - left) / 2;
+//     if (mid == num / mid) {
+//         // Confirm there is no remainder frominteger division
+//         if (num % mid == 0) return true;
+        
+//         // If there's a remainder, mid * mid isstrictly less than num
+//         left = mid + 1;
+//     } else if (mid > num / mid) {
+//         right = mid - 1;
+//     } else {
+//         left = mid + 1;
+//     }
+// }
