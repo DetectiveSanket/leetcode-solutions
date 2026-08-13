22be3978265f51f6869f3d271123d572dffdc796
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-08-13 19:31:20 +0530

    Time: 0 ms (100%), Space: 13.2 MB (61.46%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        
+/* 
+        // Brust forst approch
         vector<int> ans;
         int n = 1;
         int p = 0;
@@ -18,11 +19,6 @@ public:
             }
         }
 
-        cout << "size : " << ans.size() << endl;
-        for(auto i : ans) {
-            cout << i << " ";
-        }
-
         int remian = k - ans.size();
         int last = arr.back();
 
@@ -32,7 +28,54 @@ public:
             remian--;
         }
 
-        // return ans.empty() ? 6 : ans.back();
+        return ans.back(); 
+        
+*/
+
+// ------------------------------------ //
+
+/* 
+    // Linera approach (little better)
+        vector<int> ans;
+        int n = 1;
+        int p = 0;
+
+        while(ans.size() < k) {
+
+            if(p < arr.size() && arr[p] == n) {
+                p++;
+            }
+            else {
+                ans.push_back(n);
+            }
+            n++;
+        }
+
         return ans.back();
+*/
+
+// ----------------------------------//
+
+  // Approach :- Binary search
+  // Time :- O(log n)
+  // Space :- O(1);
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while(left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] - (mid + 1) < k) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+
+        }
+
+        return left + k;
     }
 };
