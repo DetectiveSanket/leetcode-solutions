1d44a3f266b3963078e3c84b8c6ebb60c201ccf4
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-08-16 19:51:19 +0530

    Time: 848 ms (5.09%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,6 +1,23 @@
 SELECT 
     request_at AS Day,
     ROUND(
+        SUM(CASE WHEN status != 'completed' THEN 1.0 ELSE 0.0 END) / COUNT(*),
+        2
+    ) AS [Cancellation Rate]
+FROM Trips
+WHERE 
+    request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    AND client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
+    AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
+GROUP BY request_at;
+
+
+/* 
+## MySQL
+
+SELECT 
+    request_at AS Day,
+    ROUND(
         SUM(IF(status != 'completed', 1, 0)) / COUNT(*), 
         2
     ) AS "Cancellation Rate"
@@ -10,3 +27,5 @@ WHERE
     AND client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
     AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
 GROUP BY request_at;
+
+ */
