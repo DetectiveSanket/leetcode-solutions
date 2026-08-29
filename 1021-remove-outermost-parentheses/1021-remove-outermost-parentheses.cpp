85d2287ec89dc2413ecfe26ce4fbdb37e129156e
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-08-29 18:35:57 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.67%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -3,35 +3,54 @@ public:
     string removeOuterParentheses(string s) {
 
   // Approach :- Stack - Parentheses
+  // Time :- O(N^2)
+  // Space :- O(N) 
+
+        // int open = 0;
+        // int close = 0;
+        // string ans = "";
+        // string temp = "";
+
+        // for(char c : s) {
+
+        //     if(c == '(') {
+        //         temp.push_back(c);
+        //         open++;
+        //     }
+
+        //     else {
+        //         temp.push_back(c);
+        //         close++;
+        //     }
+
+        //     if(open == close) {
+        //         temp.erase(0,1);
+        //         temp.pop_back();
+        //         ans += temp;
+        //         temp.clear();
+        //     }
+
+        // }
+
+        // return ans;
+
+  // Approach :- Stack - Parentheses
   // Time :- O(N)
   // Space :- O(N) 
 
-        int open = 0;
-        int close = 0;
         string ans = "";
-        string temp = "";
+        int open = 0;
 
         for(char c : s) {
-
-            if(c == '(') {
-                temp.push_back(c);
+            if( c == '(') {
+                if(open > 0)  ans += c;
                 open++;
             }
-
             else {
-                temp.push_back(c);
-                close++;
+                open--;
+                if(open > 0) ans += c;
             }
-
-            if(open == close) {
-                temp.erase(0,1);
-                temp.pop_back();
-                ans += temp;
-                temp.clear();
-            }
-
         }
-
         return ans;
     }
 };
