15a702f67c1755af0f6a2ef585dff8eba91801cb
Sanket Talekar <141250234+DetectiveSanket@users.noreply.github.com>
2026-09-14 18:55:04 +0530

    Time: 2 ms (87.42%), Space: 47.4 MB (17.94%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -12,36 +12,55 @@ class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
 
-  // Approach :- 
-  // Time :- 
-  // Space :-  
+  // Approach :- monotonic stack 
+  // Time :- O(N)
+  // Space :- O(N)
 
-        int n = 0;
-        ListNode* temp = head;
+        // int n = 0;
+        // ListNode* temp = head;
+        // while(temp != nullptr) {
+        //     n++;
+        //     temp = temp -> next;
+        // }
+
+        // stack<pair<int,int>> st;
+        // vector<int> ans (n , 0);
+        // temp = head;
+
+        // int i = 0;
+        // while(temp != nullptr) {
+
+        //     while(!st.empty() && temp -> val > st.top().first) {
+        //        int ind = st.top().second;
+        //        ans[ind] = temp -> val;
+        //        st.pop();
+        //     }
+
+        //     st.push({temp -> val , i});
+        //     temp = temp -> next;
+        //     i++;
+        // }
+
+        // return ans;
+
+        //> convert the linkedList into vector
+        ListNode *temp = head;
+        vector<int> arr;
         while(temp != nullptr) {
-            n++;
+            arr.push_back(temp -> val);
             temp = temp -> next;
         }
-
-        stack<pair<int,int>> st;
-        vector<int> ans (n , 0);
-        temp = head;
-
-        int i = 0;
-        while(temp != nullptr) {
-
-            while(!st.empty() && temp -> val > st.top().first) {
-               int ind = st.top().second;
-               ans[ind] = temp -> val;
-               st.pop();
+        
+        stack<int> st;
+        vector<int> ans(arr.size() , 0);
+        for(int i = 0; i < arr.size(); i++) {
+            while(!st.empty() && arr[i] > arr[st.top()] ) {
+                ans[st.top()] = arr[i];
+                st.pop();
             }
-
-            st.push({temp -> val , i});
-            temp = temp -> next;
-            i++;
+            st.push(i);
         }
 
         return ans;
-
     }
 };
